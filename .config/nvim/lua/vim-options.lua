e946827fc73a6082cff6bf1d215e016d807e91a8
Imnot_EdMateo <mateolopez899@outlook.com>
2024-03-13 21:36:04 -0500

    feat: primordial files

---
+++ .config/nvim/lua/vim-options.lua
@@ -0,0 +1,7 @@
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set expandtab")
+vim.cmd("set clipboard+=unnamedplus")
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "number"
---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+~/.fehbg &
+greenclip daemon &
+picom &
