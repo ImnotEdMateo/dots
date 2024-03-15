8da5b2979f85fc7bb43dd7ee40ab64ee8d192ff8
Imnot_EdMateo <mateolopez899@outlook.com>
2024-03-15 09:24:04 -0500

    feat: cursorline

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -2,6 +2,7 @@ vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set expandtab")
 vim.cmd("set clipboard+=unnamedplus")
+vim.cmd("set cursorline")
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
