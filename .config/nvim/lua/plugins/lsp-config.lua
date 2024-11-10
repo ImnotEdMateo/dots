6f882db782c07bbaa16ae8214dbabe35db5e2cb1
ImnotEdMateo <mateolopez899@outlook.com>
2024-11-10 15:39:56 -0500

    fix(LSP): FUCKING warning in css files about tailwind marks

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -9,39 +9,42 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
-        require("mason-lspconfig").setup({
-            ensure_installed = {"cssls", "tsserver", "tailwindcss", "emmet_ls"},
-        })
+      require("mason-lspconfig").setup({
+        ensure_installed = {"cssls", "ts_ls", "tailwindcss", "emmet_ls", ""},
+      })
     end
   },  
 
   {
     "neovim/nvim-lspconfig",
     config = function()
-        local lspconfig = require('lspconfig')
-        local capabilities = require('cmp_nvim_lsp').default_capabilities()
-        lspconfig.cssls.setup({
-            capabilities = capabilities
-        })
+      local lspconfig = require('lspconfig')
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      lspconfig.cssls.setup({
+        capabilities = capabilities
+        settings = {
+          css = { 
+            lint = {
+              unknownAtRules = "ignore"
+            }
+          }
+        }
+      })
 
-        lspconfig.tailwindcss.setup({
-            capabilities = capabilities
-        })
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities
+      })
 
-        lspconfig.tsserver.setup({
-            capabilities = capabilities
-        })
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities
+      })
 
-        lspconfig.emmet_ls.setup({
-            capabilities = capabilities,
-            filetypes = { 'html', 'css', 'javascript', 'javascriptreact', 'typescriptreact', 'astro' },
-        })
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities,
+        filetypes = { 'html', 'css', 'javascript', 'javascriptreact', 'typescriptreact', 'astro' },
+      })
 
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
     end
-  },
-
-  {
-    "vlime/vlime",
   },
 }
