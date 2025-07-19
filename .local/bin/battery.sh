792d449ff03f938652ed37c300c4ba800dfff6c5
ImnotEdMateo <mateolopez899@outlook.com>
2025-07-19 18:20:11 -0500

    fix: picom config simplied and battery script icon appears if <= 20

--- .local/bin/battery.sh
+++ .local/bin/battery.sh
@@ -21,21 +21,21 @@ if [ "$AC_STATUS" -eq 1 ]; then
     ICON="^c#689d6a^󰂄"  # Símbolo de carga
 else
     if [ "$BAT" -ge 80 ]; then
-        ICON="^c#98971A^󰁹"  # Batería llena
+        ICON="^c#98971A^󰁹"
     elif [ "$BAT" -ge 70 ]; then
-        ICON="^c#98971A^󰂁"  # Batería llena
+        ICON="^c#98971A^󰂁"
     elif [ "$BAT" -ge 60 ]; then
-      ICON="^c#98971A^󰂀"  # Batería 3/4
+        ICON="^c#98971A^󰂀"  
     elif [ "$BAT" -ge 50 ]; then
-        ICON="^c#98971A^󰁿"  # Batería 1/2
+        ICON="^c#98971A^󰁿"  
     elif [ "$BAT" -ge 40 ]; then
-        ICON="^c#98971A^󰁾"  # Batería 1/4
+        ICON="^c#98971A^󰁾" 
     elif [ "$BAT" -ge 30 ]; then
-        ICON="^c#98971A^󰁽"  # Batería 1/4
+        ICON="^c#98971A^󰁽" 
     elif [ "$BAT" -ge 20 ]; then
-        ICON="^c#D79921^󰁼"  # Batería 1/4
-    elif [ "$BAT" -ge 10 ]; then
-        ICON="^c#FB4934^󰁻"  # Batería 1/4
+        ICON="^c#D79921^󰁼" 
+    else
+        ICON="^c#FB4934^󰁻" 
     fi
 fi
 
